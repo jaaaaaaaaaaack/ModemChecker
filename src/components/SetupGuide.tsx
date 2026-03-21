e7a40c04f5c89c8768b9713fd76b68ac4c20dc59
Jack Howard <jackthoward@gmail.com>
2026-03-22 00:46:35 +1100

    Wire up 7 new modems in SetupGuide dev menu
    All 15 modems now available for testing in the dev menu dropdown.
    New imports: NetComm NF18ACV, DrayTek Vigor 2862, Synology RT6600ax,
    TP-Link Deco X60, Huawei HG659, Sagemcom F@ST 5366 TN, TP-Link
    Archer VR2100v.

--- src/components/SetupGuide.tsx
+++ src/components/SetupGuide.tsx
@@ -28,6 +28,13 @@ import dlinkData from "../../data/setup-guides/d-link-dsl-2888a.json";
 import linksysData from "../../data/setup-guides/linksys-velop-mx4200.json";
 import netgearData from "../../data/setup-guides/netgear-nighthawk-rax50.json";
 import telstraData from "../../data/setup-guides/telstra-smart-modem-gen-3.json";
+import netcommData from "../../data/setup-guides/netcomm-nf18acv.json";
+import draytekData from "../../data/setup-guides/draytek-vigor-2862.json";
+import synologyData from "../../data/setup-guides/synology-rt6600ax.json";
+import decoData from "../../data/setup-guides/tp-link-deco-x60.json";
+import huaweiData from "../../data/setup-guides/huawei-hg659.json";
+import sagemcomData from "../../data/setup-guides/sagemcom-fast-5366-tn.json";
+import vr2100Data from "../../data/setup-guides/tp-link-archer-vr2100v.json";
 
 const ALL_MODEMS = [
   tpLinkData,
@@ -38,6 +45,13 @@ const ALL_MODEMS = [
   linksysData,
   netgearData,
   telstraData,
+  netcommData,
+  draytekData,
+  synologyData,
+  decoData,
+  huaweiData,
+  sagemcomData,
+  vr2100Data,
 ] as const;
 
 type TechType = "fttp" | "fttc" | "hfc" | "fttn" | "fttb";
